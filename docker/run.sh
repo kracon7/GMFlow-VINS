c35589625fd2c4742106a96e144a10c11bc0db1f
Jiacheng Yuan <ethancooper007@gmail.com>
2023-11-27 10:48:43 -0800

    Ready to test D455 data

--- docker/run.sh
+++ docker/run.sh
@@ -1,61 +1,47 @@
 #!/bin/bash
-trap : SIGTERM SIGINT
 
-function abspath() {
-    # generate absolute path from relative path
-    # $1     : relative filename
-    # return : absolute path
-    if [ -d "$1" ]; then
-        # dir
-        (cd "$1"; pwd)
-    elif [ -f "$1" ]; then
-        # file
-        if [[ $1 = /* ]]; then
-            echo "$1"
-        elif [[ $1 == */* ]]; then
-            echo "$(cd "${1%/*}"; pwd)/${1##*/}"
-        else
-            echo "$(pwd)/$1"
-        fi
-    fi
-}
+# Dockerfile builds a container with ROS and gnss_comm package.
+# This file launches the container and bind GVINS source dir and 
+# dataset dir with the container.
+#
+# The source dir is read only inside the container and changes
+# from the host will show up inside the container. So develepment
+# can be performed from the host side.
+# 
+# * After launching the container
+# 1. run 'catkin build' to build the GVINS packages
+# 2. 'source devel/setup.bash'
+#
+# * Enter the container from another terminal
+# docker exec -it gvins /bin/bash
 
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 LAUNCH_FILE" >&2
-  exit 1
-fi
+# IMPORTANT SETUP STEPS:
+# In order to run RViz inside the container, the X server with 
+# GPU must be properly setup.
+# 1. The args in this files are necessary
+# 2. Install nvidia-container-toolkit and follow steps below:
+#    sudo nvidia-ctk runtime configure --runtime=docker
+#    sudo systemctl restart docker
 
-roscore &
-ROSCORE_PID=$!
-sleep 1
+GVINS_DIR=$HOME/code/catkin_ws/src/GMFlow-VINS
 
-rviz -d ../config/gvins_rviz_config.rviz &
-RVIZ_PID=$!
+echo $GVINS_DIR
 
-GVINS_DIR=$(abspath "..")
+xhost +local:docker 
 
 docker run \
   -it \
   --rm \
   --net=host \
-  -v ${GVINS_DIR}:/root/catkin_ws/src/GVINS/ \
+  --privileged \
+  --env=NVIDIA_VISIBLE_DEVICES=all \
+  --env=NVIDIA_DRIVER_CAPABILITIES=all \
+  --env=QT_X11_NO_MITSHM=1 \
+  --gpus all \
+  --name gvins \
+  -e DISPLAY=$DISPLAY \
+  -v /tmp/.X11-unix/:/tmp/.X11-unix:ro \
+  -v $GVINS_DIR:/root/catkin_ws/src/GMFlow-VINS/:ro \
+  -v $HOME/tmp:/root/tmp/ \
   gvins:latest \
-  /bin/bash -c \
-  "cd /root/catkin_ws/; \
-  catkin config \
-        --env-cache \
-        --extend /opt/ros/$ROS_DISTRO \
-       --cmake-args \
-         -DCMAKE_BUILD_TYPE=Release; \
-     catkin build; \
-     source devel/setup.bash; \
-     roslaunch gvins ${1}"
-
-wait $ROSCORE_PID
-wait $RVIZ_PID
-
-if [[ $? -gt 128 ]]
-then
-    kill $ROSCORE_PID
-    kill $RVIZ_PID
-fi
+  /bin/bash
